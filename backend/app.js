b4642a3a13995c77309a3a7e5c0c99f7a1f34b38
Gowtham Behara <gothsiddu@gmail.com>
2018-10-05 14:37:23 +0530

    Include code for mongodb connection

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const config = require('./db');
+
+mongoose.connect(config.DB, { useNewUrlParser: true})
+    .then(() => {
+        console.log("MongoDB connected");
+    }).catch(err => {
+        console.log("MongoDB connection failed!")
+    });
 
 const app = express();
 
