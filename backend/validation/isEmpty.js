12389e3ffbb4f002301dd347eebc19f99ddf6000
Gowtham Behara <gothsiddu@gmail.com>
2018-10-05 14:45:06 +0530

    Create validation functions

---
+++ backend/validation/isEmpty.js
@@ -0,0 +1,10 @@
+const isEmpty = (value) => {
+    return (
+        undefined === value ||
+        null == value ||
+        (typeof value === 'object' && Object.keys(value).length === 0) ||
+        (typeof value === 'string' && value.trim().length === 0)
+    );
+};
+
+module.exports = isEmpty;
