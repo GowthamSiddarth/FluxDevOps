f5e4b99d2dfd67c5f9303bdb7db65fc31efeb98e
Gowtham Behara <gothsiddu@gmail.com>
2018-10-06 00:13:42 +0530

    Create custom Navbar and import in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        React Redux Auth App
+        <Navbar />
       </div>
     );
   }
