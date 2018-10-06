82950753b3e02b78d8e358fe03c7fc1a62d5901e
Gowtham Behara <gothsiddu@gmail.com>
2018-10-06 15:53:43 +0530

    use className instead of class attr for all tags

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,15 +6,15 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <Link className="navbar-brand" href="/" to="/">Redux Node Auth</Link>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
                             <Link className="nav-link" href="/" to="/">Home</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <Link className="nav-link" href="/register" to="/register">Register</Link>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <Link className="nav-link" href="/login" to="/login">Login</Link>
                         </li>
                     </ul>
