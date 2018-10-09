b97bad6a7c41bc2ed753968a58b4a099544abf64
Gowtham Behara <gothsiddu@gmail.com>
2018-10-09 17:23:58 +0530

    Retrieve jobs on component load

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 
+import { getJenkinsJobs } from '../actions/jenkins';
+
 class Home extends Component {
+
+    componentWillMount() {
+        this.props.getJenkinsJobs();
+    }
 
     componentDidMount() {
         if (!this.props.auth.isAuthenticated) {
@@ -12,20 +19,30 @@ class Home extends Component {
     }
 
     render() {
+        console.log(this.props.jenkins.jobs);
         return (
-            <div>
-                Welcome {localStorage.getItem('name')}
+            <div className="container" style={{ marginTop: '50px', width: '700px' }}>
+                <h2 style={{ marginBottom: '40px' }}> Welcome {localStorage.getItem('name')}</h2>
+
             </div>
         );
     }
 }
 
 Home.propTypes = {
+    getJenkinsJobs: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
-}
+    jenkins: PropTypes.object.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+    getJenkinsJobs: bindActionCreators(getJenkinsJobs, dispatch),
+});
+
 
 const mapStateToProps = (state) => ({
     auth: state.auth,
+    jenkins: state.jenkins
 });
 
-export default connect(mapStateToProps, {})(withRouter(Home));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Home));
