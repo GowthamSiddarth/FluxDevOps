68675bd39e47079e1000b9bb2978b9d2a8da56a9
Gowtham Behara <gothsiddu@gmail.com>
2018-10-09 12:26:26 +0530

    Get jenkins jobs for auth user

---
+++ backend/routes/Jenkins.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const passport = require('passport');
+const http = require('http');
+const links = require('../links');
+
+const router = express.Router();
+
+router.post('/jobs', passport.authenticate('jwt', { session: false }), (req, res) => {    
+    http.get(links.jenkins + '/api/json?pretty=true', (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            res.json(JSON.parse(data).jobs.map((job) => {
+                return {
+                    name: job.name,
+                    url: job.url
+                }
+            }));
+        });
+    }).on("error", (err) => {
+        console.log(err.message);
+        res.status(400).json({ success: false, message: err.message })
+    });
+});
+
+module.exports = router;
