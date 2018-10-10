6a854a917984ac039027aad933c232f50722cc79
Gowtham Behara <gothsiddu@gmail.com>
2018-10-10 17:19:33 +0530

    Create new component for Project Type

---
+++ frontend/src/components/ProjectType.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProjectType extends Component {
+    render() {
+        return(
+            <div>Select Project Type</div>
+        );
+    }
+}
+
+export default ProjectType;
