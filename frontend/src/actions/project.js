a139fbaf8335cb318e7bb6b0c01994243b7e9175
Gowtham Behara <gothsiddu@gmail.com>
2018-10-10 17:14:20 +0530

    Implement action to backend for prj loc input

--- frontend/src/actions/project.js
+++ frontend/src/actions/project.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE } from './types';
 
-export const createNewProject = (location) => dispatch => {
-    axios.post('/api/jenkins/createNewProject', location)
+export const createNewProject = (location, history) => dispatch => {
+    axios.post('/api/projects/createNewProject', location)
         .then(res => {
+            console.log(res.data);
             dispatch({
                 type: res.data.success ? FETCH_DATA_SUCCESS : FETCH_DATA_FAILURE,
                 payload: res.data.message,
             })
+            history.push('/projectCreated');            
         })
         .catch(err => {
             dispatch({
