3402afb694dd4ac6941f1c611f2ee827b5364ee3
Gowtham Behara <gothsiddu@gmail.com>
2018-10-10 17:22:20 +0530

    Remove logger

--- frontend/src/actions/project.js
+++ frontend/src/actions/project.js
@@ -4,7 +4,6 @@ import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE } from './types';
 export const createNewProject = (location, history) => dispatch => {
     axios.post('/api/projects/createNewProject', location)
         .then(res => {
-            console.log(res.data);
             dispatch({
                 type: res.data.success ? FETCH_DATA_SUCCESS : FETCH_DATA_FAILURE,
                 payload: res.data.message,
