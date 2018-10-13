331a0ef22b6bb344f2d0dff6e763e15dcf2c3e3d
Gowtham Behara <gothsiddu@gmail.com>
2018-10-13 22:29:10 +0530

    Use jenkins token to auth url

--- backend/routes/Jenkins.js
+++ backend/routes/Jenkins.js
@@ -27,28 +27,6 @@ router.get('/jobs', passport.authenticate('jwt', { session: false }), (req, res)
             })});
         }
     });
-    /*
-    http.get(links.jenkins + '/api/json?pretty=true', (response) => {
-        let data = '';
-
-        response.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        response.on('end', () => {
-            console.log(data);
-            res.json({success: true, message: JSON.parse(data).jobs.map((job) => {
-                return {
-                    name: job.name,
-                    url: job.url
-                }
-            })});
-        });
-    }).on("error", (err) => {
-        console.log(err.message);
-        res.status(400).json({ success: false, message: err.message })
-    });
-    */
 });
 
 module.exports = router;
