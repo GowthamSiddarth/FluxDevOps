ab9446f4a555a2bbcf674deb4b7321bf93d2fc5b
Gowtham Behara <gothsiddu@gmail.com>
2018-10-13 21:57:39 +0530

    Use jenkinsapis to get list of all jobs

--- backend/routes/Jenkins.js
+++ backend/routes/Jenkins.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const passport = require('passport');
 const http = require('http');
+const jenkinsapi = require('jenkins-api');
+
 const links = require('../links');
+const secrets = require('../secrets');
+const remoteTokenLogin = require('../remote-token-login');
 
 const router = express.Router();
 
-router.get('/jobs', passport.authenticate('jwt', { session: false }), (req, res) => {    
+const jenkinsToken = secrets.jenkinsToken;
+const jenkinsAuthUrl = remoteTokenLogin(links.jenkins, secrets.jenkinsUser, secrets.jenkinsToken);
+const jenkins = jenkinsapi.init(jenkinsAuthUrl);
+
+router.get('/jobs', passport.authenticate('jwt', { session: false }), (req, res) => {
+    jenkins.all_jobs({token: 'jenkins-token'}, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({message: 'Some server error occurred'});
+        } else {
+            res.json({success: true, message: data.map((job) => {
+                return {
+                    name: job.name,
+                    url: job.url
+                }
+            })});
+        }
+    });
+    /*
     http.get(links.jenkins + '/api/json?pretty=true', (response) => {
         let data = '';
 
@@ -14,6 +36,7 @@ router.get('/jobs', passport.authenticate('jwt', { session: false }), (req, res)
         });
 
         response.on('end', () => {
+            console.log(data);
             res.json({success: true, message: JSON.parse(data).jobs.map((job) => {
                 return {
                     name: job.name,
@@ -25,6 +48,7 @@ router.get('/jobs', passport.authenticate('jwt', { session: false }), (req, res)
         console.log(err.message);
         res.status(400).json({ success: false, message: err.message })
     });
+    */
 });
 
 module.exports = router;
