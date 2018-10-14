2ebd68599b9b3c92f10878ffd3ba216d31ae5bca
Gowtham Behara <gothsiddu@gmail.com>
2018-10-14 23:27:53 +0530

    Validate projectName and save

--- backend/routes/Project.js
+++ backend/routes/Project.js
@@ -6,7 +6,7 @@ const Project = require('../models/Project');
 const router = express.Router();
 
 router.post('/createNewProject', passport.authenticate('jwt', { session: false }), function (req, res) {
-    const { errors, isValid } = validateProjectDetails(req.body);    
+    const { errors, isValid } = validateProjectDetails(req.body);
 
     if (!isValid) {
         res.status(400).json(errors);
@@ -16,24 +16,34 @@ router.post('/createNewProject', passport.authenticate('jwt', { session: false }
         location: req.body.projectLocation
     }).then(project => {
         if (project) {
-            errors.projectLocation = 'Job for Project already created!';
+            errors.projectLocation = 'Job for Project Location already created!';
             return res.status(400).json(errors);
         } else {
-            const newProject = new Project({
-                location: req.body.projectLocation,
-                type: req.body.projectType,
-            });
-
-            newProject.save()
-                .then(project =>{                                       
-                    res.json({
-                        success: true,
-                        message: {
-                            exists: false
-                        }
+            Project.findOne({
+                name: req.body.projectName
+            }).then(project => {
+                if (project) {
+                    errors.projectName = 'Job for Project Name already created!';
+                    return res.status(400).json(errors);
+                } else {
+                    const newProject = new Project({
+                        location: req.body.projectLocation,
+                        name: req.body.projectName,
+                        type: req.body.projectType,
                     });
-                })
-                .catch(err => console.log(err));
+
+                    newProject.save()
+                        .then(project => {
+                            res.json({
+                                success: true,
+                                message: {
+                                    exists: false
+                                }
+                            });
+                        })
+                        .catch(err => console.log(err));
+                }
+            }).catch(err => console.error(err));
         }
     }).catch(err => console.log(err));
 });
