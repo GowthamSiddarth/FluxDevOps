da6e88fd35a070e2aa50183dd456bb425a025102
Gowtham Behara <gothsiddu@gmail.com>
2018-10-15 00:32:30 +0530

    Implement api to create jenkins job

--- backend/routes/Jenkins.js
+++ backend/routes/Jenkins.js
@@ -2,6 +2,8 @@ const express = require('express');
 const passport = require('passport');
 const http = require('http');
 const jenkinsapi = require('jenkins-api');
+const fs = require('fs');
+const xml2js = require('xml2js');
 
 const links = require('../links');
 const secrets = require('../secrets');
@@ -14,18 +16,45 @@ const jenkinsAuthUrl = remoteTokenLogin(links.jenkins, secrets.jenkinsUser, secr
 const jenkins = jenkinsapi.init(jenkinsAuthUrl);
 
 router.get('/jobs', passport.authenticate('jwt', { session: false }), (req, res) => {
-    jenkins.all_jobs({token: 'jenkins-token'}, (err, data) => {
+    jenkins.all_jobs({ token: 'jenkins-token' }, (err, data) => {
         if (err) {
             console.log(err);
-            res.status(500).json({message: 'Some server error occurred'});
+            res.status(500).json({ message: 'Some server error occurred' });
         } else {
-            res.json({success: true, message: data.map((job) => {
-                return {
-                    name: job.name,
-                    url: job.url
-                }
-            })});
+            res.json({
+                success: true, message: data.map((job) => {
+                    return {
+                        name: job.name,
+                        url: job.url
+                    }
+                })
+            });
         }
+    });
+});
+
+router.get('/createNewJob/:projectName', (req, res) => {
+    fs.readFile(__dirname + '/../jenkins-job-config.xml', (err, data) => {
+        const xmlParser = new xml2js.Parser();
+        xmlParser.parseString(data, (err, json) => {
+            json['flow-definition']['displayName'] = req.params.projectName
+
+            const xmlBuilder = new xml2js.Builder();
+            const configXml = xmlBuilder.buildObject(json);
+            jenkins.create_job(req.params.projectName, configXml, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({ message: 'Some server error occurred' });
+                } else {
+                    res.json({
+                        success: true, message: {
+                            name: data.name,
+                            url: data.url
+                        }
+                    });
+                }
+            });
+        });
     });
 });
 
