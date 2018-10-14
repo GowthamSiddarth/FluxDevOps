741e26f27ea0ed95407f343df1cdfe4489a127f0
Gowtham Behara <gothsiddu@gmail.com>
2018-10-15 01:27:15 +0530

    Add new action for creating new job

--- frontend/src/actions/jenkins.js
+++ frontend/src/actions/jenkins.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE } from './types';
+import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_CREATION_FAILURE } from './types';
 
 export const getJenkinsJobs = () => dispatch => {
     axios.get('/api/jenkins/jobs')
@@ -13,6 +13,22 @@ export const getJenkinsJobs = () => dispatch => {
             dispatch({
                 type: GET_ERRORS,
                 payload: err.response.data
+            })
+        });
+}
+
+export const createNewJob = (jobDetails) => dispatch => {
+    axios.post('/api/jenkins/createNewJob', jobDetails)
+        .then(res => {
+            dispatch({
+                type: res.data.success ? JOB_CREATION_SUCCESS : JOB_CREATION_FAILURE,
+                payload: res.data.message,
+            })                   
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
             })
         });
 }
