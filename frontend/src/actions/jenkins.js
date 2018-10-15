c7fc1cc42cd2b96fc22f4f66cf0b3b9b76552eb1
Gowtham Behara <gothsiddu@gmail.com>
2018-10-15 08:59:09 +0530

    Create new action for scheduling build

--- frontend/src/actions/jenkins.js
+++ frontend/src/actions/jenkins.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_CREATION_FAILURE } from './types';
+import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_CREATION_FAILURE, BUILD_CREATION_SUCCESS, BUILD_CREATION_FAILURE } from './types';
 
 export const getJenkinsJobs = () => dispatch => {
     axios.get('/api/jenkins/jobs')
@@ -23,7 +23,23 @@ export const createNewJob = (jobDetails) => dispatch => {
             dispatch({
                 type: res.data.success ? JOB_CREATION_SUCCESS : JOB_CREATION_FAILURE,
                 payload: res.data.message,
-            })                   
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        });
+}
+
+export const scheduleBuild = (jobDetails) => dispatch => {
+    axios.post('/api/jenkins/scheduleBuild', jobDetails)
+        .then(res => {
+            dispatch({
+                type: res.data.success ? BUILD_CREATION_SUCCESS : BUILD_CREATION_FAILURE,
+                payload: res.data.message,
+            })
         })
         .catch(err => {
             dispatch({
