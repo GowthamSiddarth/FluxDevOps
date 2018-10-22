176bb95457d165d5e5f35c6d6ea5905f87438df9
Gowtham Behara <gothsiddu@gmail.com>
2018-10-22 19:26:49 +0530

    Handle the data dispatched during jobDetails fetch action

--- frontend/src/reducers/jenkinsReducer.js
+++ frontend/src/reducers/jenkinsReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS } from '../actions/types';
+import { FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_DETAILS_SUCCESS } from '../actions/types';
 
 export default function (state = [], action) {
     switch (action.type) {
@@ -11,6 +11,12 @@ export default function (state = [], action) {
             return {
                 ...state,
                 job: action.payload
+            }
+        case JOB_DETAILS_SUCCESS:
+            return {
+                ...state,
+                buildCommand: action.payload.buildCommand,
+                deployCommand: action.payload.deployCommand,
             }
         default: return { ...state }
     }
