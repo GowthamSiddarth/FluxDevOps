921cd8e1c0a2ab8e3d6540bce3161e3a443cb4c4
Gowtham Behara <gothsiddu@gmail.com>
2018-10-22 15:49:43 +0530

    Don't include projectReducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import errorReducer from './errorReducer';
 import authReducer from './authReducer';
 import jenkinsReducer from './jenkinsReducer';
-import projectReducer from './projectReducer';
 
 export default combineReducers({
     errors: errorReducer,
     auth: authReducer,
     jenkins: jenkinsReducer,
-    project: projectReducer,
 });
