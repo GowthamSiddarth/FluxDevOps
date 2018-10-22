77501d98b0cf97310a5994031e5b4caa71c26a12
Gowtham Behara <gothsiddu@gmail.com>
2018-10-22 21:12:17 +0530

    Add input fields for changing default build & deploy commands

--- frontend/src/components/ProjectConfiguration.js
+++ frontend/src/components/ProjectConfiguration.js
@@ -3,10 +3,25 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
+import classnames from "classnames";
 
 import { getJobDetails } from "../actions/jenkins";
+import { HorizontalCenterView } from '../views/HorizontalCenterView';
 
 class ProjectConfiguration extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            buildCommand: '',
+            deployCommand: '',
+            errors: {},
+        }
+
+        this.handleBuildCommandChange = this.handleBuildCommandChange.bind(this);
+        this.handleDeployCommandChange = this.handleDeployCommandChange.bind(this);
+    }
 
     componentWillMount() {
         this.props.getJobDetails(this.props.match.params.projectName);
@@ -18,13 +33,66 @@ class ProjectConfiguration extends Component {
         }
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.jenkins) {
+            this.setState({
+                buildCommand: nextProps.jenkins.buildCommand,
+                deployCommand: nextProps.jenkins.deployCommand,
+            })
+        }
+    }
+
+    handleBuildCommandChange(e) {
+        e.preventDefault();
+        this.setState({
+            buildCommand: e.target.value.trim(),
+        })
+    }
+
+    handleDeployCommandChange(e) {
+        e.preventDefault();
+        this.setState({
+            deployCommand: e.target.value.trim(),
+        })
+    }
+
     render() {
-        console.log(this.props);
+        const errors = this.state.errors
         return (
-            <div>
-                {this.props.match.params.projectName}
-                {this.props.jenkins.buildCommand}
-                {this.props.jenkins.deployCommand}
+            <div style={{ marginTop: '50px', marginBottom: '80px' }}>
+                <HorizontalCenterView left="3" center="6" right="3">
+                    <HorizontalCenterView left="4" center="4" right="4">
+                        <h5 style={{ marginBottom: '50px' }}>{this.props.match.params.projectName}</h5>
+                    </HorizontalCenterView>
+                    <form >
+                        <div className="form-group">
+                            <label htmlFor="buildCommand">Change Build Command</label>
+                            <input
+                                type="text"
+                                placeholder="Enter build command"
+                                className={classnames('form-control form-control-md', {
+                                    'is-invalid': errors.projectLocation
+                                })}
+                                name="buildCommand"
+                                onChange={this.handleBuildCommandChange}
+                                value={this.state.buildCommand}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="deployCommand">Change Deploy Command</label>
+                            <input
+                                type="text"
+                                placeholder="Enter deploy command"
+                                className={classnames('form-control form-control-md', {
+                                    'is-invalid': errors.projectLocation
+                                })}
+                                name="deployCommand"
+                                onChange={this.handleDeployCommandChange}
+                                value={this.state.deployCommand}
+                            />
+                        </div>
+                    </form>
+                </HorizontalCenterView>
             </div>
         );
     }
