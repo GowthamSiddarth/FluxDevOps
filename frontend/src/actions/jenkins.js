c0a8d945446b2a4a19c6040d0b8f0a794d025010
Gowtham Behara <gothsiddu@gmail.com>
2018-10-22 21:42:22 +0530

    Make post call to configure job changes

--- frontend/src/actions/jenkins.js
+++ frontend/src/actions/jenkins.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_CREATION_FAILURE, BUILD_CREATION_SUCCESS, BUILD_CREATION_FAILURE, JOB_DETAILS_SUCCESS, JOB_DETAILS_FAILURE } from './types';
+import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_CREATION_FAILURE, BUILD_CREATION_SUCCESS, BUILD_CREATION_FAILURE, JOB_DETAILS_SUCCESS, JOB_DETAILS_FAILURE, JOB_CONFIGURATION_SUCCESS, JOB_CONFIGURATION_FAILURE } from './types';
 
 export const getJenkinsJobs = () => dispatch => {
     axios.get('/api/jenkins/jobs')
@@ -41,6 +41,23 @@ export const getJobDetails = (jobName) => dispatch => {
                 type: res.data.success ? JOB_DETAILS_SUCCESS : JOB_DETAILS_FAILURE,
                 payload: res.data.message,
             });
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        });
+}
+
+export const configureJob = (job, history) => dispatch => {
+    axios.post('/api/jenkins/configureJob', job)
+        .then(res => {
+            dispatch({
+                type: res.data.success ? JOB_CONFIGURATION_SUCCESS : JOB_CONFIGURATION_FAILURE,
+                payload: res.data.message,
+            })
+            history.replace("/home");
         })
         .catch(err => {
             dispatch({
