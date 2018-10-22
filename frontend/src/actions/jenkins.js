a9f6961a6619ee5067f1556001b058cc6365bddb
Gowtham Behara <gothsiddu@gmail.com>
2018-10-22 19:26:20 +0530

    Create method to fetch job details

--- frontend/src/actions/jenkins.js
+++ frontend/src/actions/jenkins.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_CREATION_FAILURE, BUILD_CREATION_SUCCESS, BUILD_CREATION_FAILURE } from './types';
+import { GET_ERRORS, FETCH_DATA_SUCCESS, FETCH_DATA_FAILURE, JOB_CREATION_SUCCESS, JOB_CREATION_FAILURE, BUILD_CREATION_SUCCESS, BUILD_CREATION_FAILURE, JOB_DETAILS_SUCCESS, JOB_DETAILS_FAILURE } from './types';
 
 export const getJenkinsJobs = () => dispatch => {
     axios.get('/api/jenkins/jobs')
@@ -25,6 +25,22 @@ export const createNewJob = (jobDetails, history) => dispatch => {
                 payload: res.data.message,
             });
             history.replace("/home");
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            })
+        });
+}
+
+export const getJobDetails = (jobName) => dispatch => {
+    axios.get('/api/jenkins/getJobDetails', jobName)
+        .then(res => {
+            dispatch({
+                type: res.data.success ? JOB_DETAILS_SUCCESS : JOB_DETAILS_FAILURE,
+                payload: res.data.message,
+            });
         })
         .catch(err => {
             dispatch({
